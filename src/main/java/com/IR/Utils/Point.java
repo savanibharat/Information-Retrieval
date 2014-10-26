ef631a52eb595c28e76e1fef1aec5f9739783112
BHARAT SAVANI <savanibharat@gmail.com>
2014-10-26 11:22:59 -0700

    Point Class

---
+++ src/main/java/com/IR/Utils/Point.java
@@ -0,0 +1,72 @@
+package com.IR.Utils;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Point.
+ */
+public class Point {
+
+	/** The x. */
+	private float x;
+	
+	/** The y. */
+	private float y;
+
+	/**
+	 * Instantiates a new point.
+	 *
+	 * @param x the x
+	 * @param y the y
+	 */
+	public Point(float x, float y) {
+
+		this.x = x;
+		this.y = y;
+
+	}
+
+	/**
+	 * Instantiates a new point.
+	 */
+	public Point() {
+	}
+
+	/**
+	 * Gets the x.
+	 *
+	 * @return the x
+	 */
+	public float getX() {
+		return x;
+	}
+
+	/**
+	 * Sets the x.
+	 *
+	 * @param x
+	 *            the x to set
+	 */
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	/**
+	 * Gets the y.
+	 *
+	 * @return the y
+	 */
+	public float getY() {
+		return y;
+	}
+
+	/**
+	 * Sets the y.
+	 *
+	 * @param y
+	 *            the y to set
+	 */
+	public void setY(float y) {
+		this.y = y;
+	}
+
+}
