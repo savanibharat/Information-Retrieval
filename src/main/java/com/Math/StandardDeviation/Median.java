4920de820ffe40cce0e86dfe87a098dff217b221
BHARAT SAVANI <savanibharat@gmail.com>
2014-10-28 22:44:19 -0700

    Median class blueprint

---
+++ src/main/java/com/Math/StandardDeviation/Median.java
@@ -0,0 +1,38 @@
+package com.Math.StandardDeviation;
+
+import java.util.List;
+
+public class Median {
+
+	public static double medianDouble(List<Double> list) {
+
+	}
+
+	public static double medianDouble(int[] a) {
+
+	}
+
+	public static float medianfloat(List<Float> list) {
+
+	}
+
+	public static float medianfloat(int[] a) {
+
+	}
+
+	public static int medianInt(List<Integer> list) {
+
+	}
+
+	public static int medianInt(int[] a) {
+
+	}
+
+	public static Object evenList() {
+
+	}
+
+	public static Object oddList() {
+
+	}
+}
